0606214203d75d2c28bf0f3956c7cc1e8882a582
Phạm Chánh Tín <chanhtincntt@gmail.com>
2011-07-28 08:34:54 +0700

    Đã làm xong search và search nâng cao, còn thiếu tính tổng giá trị món ăn trong giỏ hàng(ShoppingCart)

---
+++ KFC Source Code/FrontEnd MVC3/Controllers/ShoppingCartController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace FrontEnd_MVC3.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        //
+        // GET: /ShoppingCart/
+        FoodStoreEntities storeDB = new FoodStoreEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AddFoodToCart(int foodId)
+        {
+            List<ORDERDETAIL> orders = new List<ORDERDETAIL>();
+            var food = storeDB.FOODs.SingleOrDefault(f => f.ID == foodId);
+            if (Session["order"] != null)
+            {
+                orders = (List<ORDERDETAIL>)Session["order"];
+                int flag = 1;
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (orders[i].Food == foodId)
+                    {
+                        orders[i].Count++;
+                        orders[i].TotalPayment += orders[i].Price;
+                        flag = 0;
+                    }
+                }
+                if (flag == 1)
+                {
+                    ORDERDETAIL orderDetail = new ORDERDETAIL();
+                    orderDetail.FOOD = food;
+                    orderDetail.ID = orders[0].ID;
+                    orderDetail.Food = foodId;
+                    orderDetail.Count = 1;
+                    orderDetail.Price = food.Price;
+                    orderDetail.TotalPayment = food.Price;
+                    orders.Add(orderDetail);
+                    Session["order"] = orders;
+                }
+            }
+            else
+            {
+                ORDERDETAIL orderDetail = new ORDERDETAIL();
+                orderDetail.FOOD = food;
+                orderDetail.ID = 1;
+                orderDetail.Food = foodId;
+                orderDetail.Count = 1;
+                orderDetail.Price = food.Price;
+                orderDetail.TotalPayment = food.Price;
+                orders.Add(orderDetail);
+                Session["order"] = orders;
+            }
+            return PartialView("Index");
+        }
+    }
+}
---
+++ KFC Source Code/FrontEnd MVC3/Controllers/StoreController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace FrontEnd_MVC3.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        FoodStoreEntities StoreDB = new FoodStoreEntities();
+
+        public ActionResult Index()
+        {
+            var listFoodType = StoreDB.FOODTYPEs.ToList();
+            return View(listFoodType);
+        }
+        //Xem các món ăn thuộc về một loại món ăn(foodtype)
+        public ActionResult Browse(int id)
+        {
+            var listFood = StoreDB.FOODs.Where(p => p.FoodType == id).ToList();
+            return View(listFood);
+        }
+        //Xem thông tin chi tiết về một món ăn
+        public ActionResult Detail(int id)
+        {
+            var food = StoreDB.FOODs.Single(p => p.ID == id);
+            return View(food);
+        }
+        //Kết quả tìm kiếm
+        public  ActionResult SearchResult(List<FOOD> listFood )
+        {
+            return View(listFood);
+        }
+        //Tìm một loại món ăn theo tên
+        public PartialViewResult Search(string name)
+        {
+            var listFood = StoreDB.FOODs.Where(p => p.Name.Contains(name) == true);
+            return PartialView("SearchResult",listFood);
+        }
+        //Tìm kiếm nâng cao
+        public PartialViewResult SuperSearch(string name, int priMin, int priMax)
+        {
+            var listFood = new List<FOOD>();
+            if (priMin != null && priMax != null)
+            {
+                listFood = StoreDB.FOODs.Where(p => (p.Name.Contains(name) == true || p.Detail.Contains(name) == true) && (p.Price > priMin && p.Price < priMax)).ToList();
+            }
+            else if (priMin != null)
+            {
+                listFood = StoreDB.FOODs.Where(p => (p.Name.Contains(name) == true || p.Detail.Contains(name) == true) && p.Price > priMin).ToList();
+            }
+            else if (priMax != null)
+            {
+                listFood = StoreDB.FOODs.Where(p => (p.Name.Contains(name) == true || p.Detail.Contains(name) == true) && p.Price < priMax).ToList();
+            }
+            else
+            {
+                listFood = StoreDB.FOODs.Where(p => p.Name.Contains(name) == true || p.Detail.Contains(name) == true).ToList();
+            }
+            return PartialView("SearchResult", listFood);
+        }
+
+    }
+}
