51cf2553626fdef74d738260978f645dd9a06c7c
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-07-30 21:52:21 +0700

    Thay đổi lại CityController

---
+++ KFC Source Code/Model/CITYController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class CityController
+    {
+        private static FoodStoreEntities _db;
+
+        public static List<CITY> 
+    }
+}
