b1aa47916a8d9ac3a60379a4ce5952e736bd1c87
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-07-30 23:08:55 +0700

    Thêm CityController

---
+++ KFC Source Code/Model/CITYController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class CityController
+    {
+        private static FoodStoreEntities _db;
+
+        /**
+         * Xuất danh sách CITY
+         */
+        public static List<CITY> GetList()
+        {
+            _db = new FoodStoreEntities();
+            return _db.CITies.ToList();
+        }
+
+    }
+}
