16376d9cfe0e4319f2f9f6fd9155dc834d628fad
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-07-30 17:43:22 +0700

    Thêm DistrictController

---
+++ KFC Source Code/Model/DISTRICTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class DistrictController
+    {
+        public static FoodStoreEntities _db;
+
+        public static List<DISTRICT> GetListByCityId(int cityId)
+        {
+            _db = new FoodStoreEntities();
+            return _db.DISTRICTs.Where(p => p.IDCity == cityId).ToList();
+        }
+    }
+}
