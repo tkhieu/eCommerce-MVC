acb771351436e4bbdc1e4cc2155402678ff62c00
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-07-31 11:32:12 +0700

    Thêm lớp SaltEncrypt với phương thức mã hóa SHA1

---
+++ KFC Source Code/Utility/SaltEncrypt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utility
+{
+    public class SaltEncrypt
+    {
+        public static string HashCodeSHA1(string input)
+        {
+            string result = "";
+            const string salt = "!@#$%^&*$%^&*";
+            input = input + salt;
+            try
+            {
+                SHA1 hash = SHA1.Create();
+                var encoder = new ASCIIEncoding();
+                byte[] combined = encoder.GetBytes(input);
+                hash.ComputeHash(combined);
+                result = Convert.ToBase64String(hash.Hash);
+            }
+            catch (Exception ex)
+            {
+                string strerr = "Error in HashCode : " + ex.Message;
+            }
+            return result;
+        }
+    }
+}
