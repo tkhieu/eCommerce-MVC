6d9d16dbe3dda1264d0713c017c9aeb1bf6feabc
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-08-19 20:50:00 +0700

    Thêm lớp FoodType để quản lý FoodType

---
+++ KFC Source Code/Model/FOODTYPEController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class FoodTypeController
+    {
+        private static FoodStoreEntities _db;
+
+        public static List<FOODTYPE> GetList()
+        {
+            _db = new FoodStoreEntities();
+            return _db.FOODTYPEs.ToList();
+        }
+    }
+}
