a50097dd2bb0337097512af0790b0e354e1f7550
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-08-19 22:24:20 +0700

    Thêm lớp tương tác FTP

---
+++ KFC Source Code/Utility/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utility
+{
+    public class Configuration
+    {
+        public const String IMAGE_CDN_HOST = "http://localhost/ImageCDN/";
+    }
+}
