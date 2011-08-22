28eb80db9a1464cf27fc5de8222c52ee7e4381d2
Trần Kim Hiếu <kimhieuqtvn@gmail.com>
2011-08-22 17:21:53 +0700

    Add các Package cần thiết.

---
+++ eCommerce/eCommerce.WebUI/App_Start/RegisterClientValidationExtensions.cs
@@ -0,0 +1,11 @@
+using DataAnnotationsExtensions.ClientValidation;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(eCommerce.WebUI.App_Start.RegisterClientValidationExtensions), "Start")]
+ 
+namespace eCommerce.WebUI.App_Start {
+    public static class RegisterClientValidationExtensions {
+        public static void Start() {
+            DataAnnotationsModelValidatorProviderExtensions.RegisterValidationExtensions();            
+        }
+    }
+}
